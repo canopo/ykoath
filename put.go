896dd060ae925e893b674e75f65c02b052212dff
z4yx <zhangyx@imac.(none)>
2019-10-07 13:44:46 +0800

    support for more options with PUT command

--- put.go
+++ put.go
@@ -1,24 +1,35 @@
 package ykoath
 
+import "encoding/binary"
+
 // Put sends a "PUT" instruction, storing a new / overwriting an existing OATH
 // credentials with an algorithm and type, 6 or 8 digits one-time password,
 // shared secrets and touch-required bit
-func (o *OATH) Put(name string, a Algorithm, t Type, digits uint8, key []byte, touch bool) error {
+func (o *OATH) Put(name string, a Algorithm, t Type, digits uint8, key []byte, touch bool, increasing bool, counter uint32) error {
 
 	var (
 		alg = (0xf0|byte(a))&0x0f | byte(t)
 		dig = byte(digits)
 		prp []byte
+		imf []byte
 	)
 
-	if touch {
-		prp = write(0x78, []byte{0x02})
+	if touch || increasing {
+		var b2i = map[bool]byte{false: 0, true: 1}
+		prp = write(0x78, []byte{b2i[touch] | b2i[increasing]<<1})
+	}
+
+	if counter != 0 {
+		imfVal := make([]byte, 4)
+		binary.LittleEndian.PutUint32(imfVal, counter)
+		prp = write(0x7A, imfVal)
 	}
 
 	_, err := o.send(0x00, 0x01, 0x00, 0x00,
 		write(0x71, []byte(name)),
 		write(0x73, []byte{alg, dig}, key),
 		prp,
+		imf,
 	)
 
 	return err
