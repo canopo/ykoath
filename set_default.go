61734b0bacab59efb46c27b6a5c558fb0c0d703e
Yuxiang Zhang <z4yx@users.noreply.github.com>
2024-02-01 20:53:39 +0800

    Update set_default.go

--- set_default.go
+++ set_default.go
@@ -5,7 +5,11 @@ func (o *OATH) SetAsDefault(name string) error {
 	_, err := o.send(0x00, 0x55, 0x00, 0x00,
 		write(0x71, []byte(name)),
 	)
-
+	if err != nil {
+		_, err = o.send(0x00, 0x55, 0x01, 0x00,
+			write(0x71, []byte(name)),
+		)
+	}
 	return err
 
 }
